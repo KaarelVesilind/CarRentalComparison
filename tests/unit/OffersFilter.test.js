97f105a8911b70ec3d6eaa3aed159978b3a1f0ef
vimadi <vimadi@ttu.ee>
2022-10-15 12:59:08 +0300

    Offers sort test - betFriendly true - FAIL

--- tests/unit/OffersFilter.test.js
+++ tests/unit/OffersFilter.test.js
@@ -34,4 +34,19 @@ describe("Offers filter", function () {
       expectedOffersIds.sort()
     );
   });
+
+  test("betFriendly true", async () => {
+    // ARRANGE
+    const expectedOffersIds = [1, 4, 5, 6, 7, 8, 9, 10, 12, 13];
+    const offers = offersTestData;
+    filterConditionsObj.petFriendly = true;
+
+    // ACT
+    const result = offersFilter.filter(offers, filterConditionsObj);
+
+    // ASSERT
+    expect(result.map((offer) => offer.id).sort()).toEqual(
+      expectedOffersIds.sort()
+    );
+  });
 });
