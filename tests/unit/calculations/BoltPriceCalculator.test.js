58e8179b1f940eb7a9a15b5f0e28c40574314ebc
Kaarel Vesilind <kaarel.vesilind@gmail.com>
2022-10-21 22:43:26 +0300

    Bolt price calculator - Volkswagen T-Roc - 2h 50min- SUCCESS - Branch Coverage 91.66%

--- tests/unit/calculations/BoltPriceCalculator.test.js
+++ tests/unit/calculations/BoltPriceCalculator.test.js
@@ -1,6 +1,6 @@
 import { bolt } from "../../../src/providers/shortTerm/bolt";
 import BoltPriceCalculator from "../../../src/bll/calculations/priceCalculations/BoltPriceCalculator";
-describe("Beast price calculator", function () {
+describe("Bolt price calculator", function () {
   const boltPriceCalculator = new BoltPriceCalculator();
 
   test("Volkswagen T-Roc - 30min", async () => {
@@ -18,5 +18,37 @@ describe("Beast price calculator", function () {
 
     // ASSERT
     expect(result).toBe(4.5);
+  });
+  test("Volkswagen T-Roc - 1day 9h 50min", async () => {
+    // ARRANGE
+    const car = bolt.cars.find((car) => car.name === "VW T-Roc");
+    const searchParamsObj = {
+      distance: 0,
+      days: 1,
+      hours: 9,
+      minutes: 50,
+    };
+
+    // ACT
+    const result = boltPriceCalculator.calculatePrice(car, searchParamsObj);
+
+    // ASSERT
+    expect(result).toBe(61.8);
+  });
+  test("Volkswagen T-Roc -  2h 50min", async () => {
+    // ARRANGE
+    const car = bolt.cars.find((car) => car.name === "VW T-Roc");
+    const searchParamsObj = {
+      distance: 0,
+      days: 0,
+      hours: 2,
+      minutes: 50,
+    };
+
+    // ACT
+    const result = boltPriceCalculator.calculatePrice(car, searchParamsObj);
+
+    // ASSERT
+    expect(result).toBe(20.37);
   });
 });
