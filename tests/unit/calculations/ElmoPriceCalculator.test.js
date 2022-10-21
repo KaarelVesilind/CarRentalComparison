079dee50243d4c23f9b73a479259af1abc05f829
Kaarel Vesilind <kaarel.vesilind@gmail.com>
2022-10-21 23:06:20 +0300

    Elmo price calculator - Rent 1 - 28day - SUCCESS - Branch Coverage 67.64%

--- tests/unit/calculations/ElmoPriceCalculator.test.js
+++ tests/unit/calculations/ElmoPriceCalculator.test.js
@@ -3,7 +3,7 @@ import ElmoPriceCalculator from "../../../src/bll/calculations/priceCalculations
 describe("Elmo price calculator", function () {
   const elmoPriceCalculator = new ElmoPriceCalculator();
 
-  test("Rent 1 - 101km 1day 1h 1min", async () => {
+  test("Rent 1 - 3801km 38day 1h 1min", async () => {
     // ARRANGE
     const car = elmo.cars.find((car) => car.name === "Rent1");
     const searchParamsObj = {
@@ -19,5 +19,23 @@ describe("Elmo price calculator", function () {
 
     // ASSERT
     expect(Number(result.toFixed(2))).toBe(971.81);
+  });
+
+  test("Rent 1 -  28day ", async () => {
+    // ARRANGE
+    const car = elmo.cars.find((car) => car.name === "Rent1");
+    const searchParamsObj = {
+      distance: 0,
+      days: 28,
+      hours: 0,
+      minutes: 0,
+    };
+
+    // ACT
+    let result = elmoPriceCalculator.calculatePrice(car, searchParamsObj);
+    result = result.price ?? result;
+
+    // ASSERT
+    expect(Number(result.toFixed(2))).toBe(700);
   });
 });
