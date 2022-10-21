8f029cc5e5022302779357a047963cbf2c2636a9
Kaarel Vesilind <kaarel.vesilind@gmail.com>
2022-10-21 22:03:08 +0300

    CityBee price calculator - Volkswagen T-Roc - 5min - SUCCESS - Branch Coverage 68.75%

--- tests/unit/calculations/CityBeePriceCalculator.test.js
+++ tests/unit/calculations/CityBeePriceCalculator.test.js
@@ -38,4 +38,21 @@ describe("CityBee price calculator", function () {
     // ASSERT
     expect(Number(result.toFixed(2))).toBe(12.63);
   });
+
+  test("Volkswagen T-Roc - 5min", async () => {
+    // ARRANGE
+    const car = citybee.cars.find((car) => car.name === "Volkswagen T-Roc");
+    const searchParamsObj = {
+      distance: 0,
+      days: 0,
+      hours: 0,
+      minutes: 5,
+    };
+
+    // ACT
+    let result = cityBeePriceCalculator.calculatePrice(car, searchParamsObj);
+    result = result.price ?? result;
+    // ASSERT
+    expect(result).toBe(2.29);
+  });
 });
