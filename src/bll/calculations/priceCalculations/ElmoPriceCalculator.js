1bb1fea856b42dbcae5e02adad7a0883d393f8ac
Kaarel Vesilind <kaarel.vesilind@gmail.com>
2022-10-31 11:25:01 +0200

    WIP updated elmo prices and calculation

--- src/bll/calculations/priceCalculations/ElmoPriceCalculator.js
+++ src/bll/calculations/priceCalculations/ElmoPriceCalculator.js
@@ -1,88 +1,30 @@
 export default class ElmoPriceCalculator {
   calculatePrice(car, searchParamsObj) {
     const price = car.price;
-    //Months
-    let monthsCost = 0;
-    if (searchParamsObj.days >= 30) {
-      const months = Math.floor(searchParamsObj.days / 30);
-      monthsCost += months * price.month;
-      searchParamsObj.days -= months * 30;
-      searchParamsObj.distance -= months * 3000;
-    }
-    // Weeks
-    let weeksCost = 0;
-    if (searchParamsObj.days >= 7) {
-      const weeks = Math.floor(searchParamsObj.days / 7);
-      searchParamsObj.days -= weeks * 7;
-      if (
-        weeks * price.week +
-          searchParamsObj.days * price.day +
-          searchParamsObj.hours * price.hour +
-          searchParamsObj.minutes * price.minute >
-        price.month
-      ) {
-        monthsCost += price.month;
-        searchParamsObj.distance -= 3000;
-        searchParamsObj.days = 0;
-        searchParamsObj.hours = 0;
-        searchParamsObj.minutes = 0;
-      } else {
-        weeksCost += weeks * price.week;
-        searchParamsObj.distance -= weeks * 700;
-      }
-    }
-    //Days
-    let daysCost = 0;
-    if (searchParamsObj.days >= 1) {
-      if (
-        searchParamsObj.days * price.day +
-          searchParamsObj.hours * price.hour +
-          searchParamsObj.minutes * price.minute >
-        price.week
-      ) {
-        weeksCost += price.week;
-        searchParamsObj.distance -= 700;
-        searchParamsObj.days = 0;
-        searchParamsObj.hours = 0;
-        searchParamsObj.minutes = 0;
-      } else {
-        daysCost += searchParamsObj.days * price.day;
-      }
-    }
-    //Hours
-    let hoursCost = 0;
-    if (searchParamsObj.hours >= 1) {
-      if (
-        searchParamsObj.hours * price.hour +
-          searchParamsObj.minutes * price.minute >
-        price.day
-      ) {
-        daysCost += price.day;
-        searchParamsObj.hours = 0;
-        searchParamsObj.minutes = 0;
-      } else {
-        hoursCost += searchParamsObj.hours * price.hour;
-      }
-    }
-    //Minutes
-    let minutesCost = 0;
-    if (searchParamsObj.minutes >= 1) {
-      minutesCost += searchParamsObj.minutes * price.minute;
+    let distanceCost = 0;
+    if (searchParamsObj.distance > 100) {
+      distanceCost = 100 * price.km + (searchParamsObj.distance - 100) * 0.1;
+    } else {
+      distanceCost = searchParamsObj.distance * price.km;
     }
 
-    if (searchParamsObj.distance < 0) {
-      searchParamsObj.distance = 0;
-    }
-    // Distance
-    let distanceCost = 0;
-    if (monthsCost > 0 || weeksCost > 0) {
-      // Extra distance cost
-      distanceCost += searchParamsObj.distance * 0.1;
-    } else if (searchParamsObj.distance > 100) {
-      distanceCost += 100 * price.km + (searchParamsObj.distance - 100) * 0.1;
-    } else {
-      distanceCost += searchParamsObj.distance * price.km;
-    }
+    const totalTime =
+      searchParamsObj.days * 24 * 60 +
+      searchParamsObj.hours * 60 +
+      searchParamsObj.minutes;
+
+    // Time
+    let { daysCost, hoursCost, minutesCost } = this._calculateTime(
+      totalTime,
+      price
+    );
+
+    let { monthsCost, weeksCost } = this._calculateTime(
+      searchParamsObj.months * 30 * 24 * 60 +
+        searchParamsObj.weeks * 7 * 24 * 60,
+      price
+    );
+
     let extraInfo = "";
     if (monthsCost > 0 || weeksCost > 0) {
       extraInfo += ` | Contact ELMO for`;
@@ -106,5 +48,87 @@ export default class ElmoPriceCalculator {
         minutesCost +
         distanceCost,
     };
+  }
+
+  _calculatePackages(car, totalCost, searchParamsObj, totalTime, price) {
+    const packages = Array.reverse(car.packages);
+    let months = 0;
+    let weeks = 0;
+    let bundlePrice = Number.MAX_SAFE_INTEGER;
+    for (const option of packages) {
+      const packageTotalTime = option.days * 24 * 60 + option.hours * 60;
+      if (option.price < totalCost) {
+        // if fits exactly in the package
+        if (
+          searchParamsObj.distance <= option.distance &&
+          totalTime <= packageTotalTime &&
+          option.price < usePackage.price
+        ) {
+          usePackage = { ...option };
+        }
+
+        // if fits in the package with some extra time or distance
+        else {
+          let packageCostExtra = option.price;
+          // Add extra distance
+          if (searchParamsObj.distance > option.distance) {
+            packageCostExtra +=
+              (searchParamsObj.distance - option.distance) * price.km;
+          }
+          // Add extra time
+          if (totalTime > packageTotalTime) {
+            let extraTime = totalTime - packageTotalTime;
+            let extraCostTime = this._calculateTime(extraTime, price);
+            packageCostExtra +=
+              extraCostTime.daysCost +
+              extraCostTime.hoursCost +
+              extraCostTime.minutesCost;
+          }
+          if (
+            packageCostExtra < totalCost &&
+            packageCostExtra < usePackage.price
+          ) {
+            usePackage = { ...option };
+            usePackage.price = packageCostExtra;
+          }
+        }
+      }
+    }
+    return usePackage;
+  }
+
+  // TODO REFACTOR - same as CityBeePriceCalculator
+  _calculateTime(totalTime, price) {
+    let days = Math.floor(totalTime / 1440);
+    totalTime -= days * 1440;
+    let hours = Math.floor(totalTime / 60);
+    totalTime -= hours * 60;
+    let minutes = totalTime;
+    // Days
+    let daysCost = 0;
+    if (days >= 1) {
+      daysCost += days * price.day;
+    }
+    // Hours
+    let hoursCost = 0;
+    if (hours >= 1) {
+      if (hours * price.hour + minutes * price.minute > price.day) {
+        daysCost += price.day;
+        hours = 0;
+        minutes = 0;
+      } else {
+        hoursCost += hours * price.hour;
+      }
+    }
+    // Minutes
+    let minutesCost = 0;
+    if (minutes >= 1) {
+      if (minutes * price.minute > price.hour) {
+        hoursCost += price.hour;
+      } else {
+        minutesCost += minutes * price.minute;
+      }
+    }
+    return { daysCost, hoursCost, minutesCost };
   }
 }
