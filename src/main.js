85927dba1209f60d0941e9c3385d75427398a107
Kaarel Vesilind <kaarel.vesilind@gmail.com>
2022-11-05 23:19:18 +0200

    added google analytics events for testing

--- src/main.js
+++ src/main.js
@@ -7,9 +7,23 @@ import router from "./router";
 import "./assets/main.css";
 
 const app = createApp(App);
-app.use(VueGtag, {
-  config: { id: import.meta.env.VITE_GOOGLE_ANALYTICS_ID },
-});
+app.use(
+  VueGtag,
+  {
+    appName: "Rentalize app",
+    pageTrackerScreenviewEnabled: true,
+    config: { id: import.meta.env.VITE_GOOGLE_ANALYTICS_ID },
+    router,
+    // for testing
+    onBeforeTrack() {
+      console.log("onBeforeTrack");
+    },
+    onAfterTrack() {
+      console.log("onAfterTrack");
+    },
+  },
+  router
+);
 app.use(createPinia());
 app.use(router);
 
