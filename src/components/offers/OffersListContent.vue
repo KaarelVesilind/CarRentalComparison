371ee3c53276e67616bf4219dc441ba67e5a2321
vimadi <vimadi@ttu.ee>
2022-11-06 17:33:35 +0200

    Listing content border radius added

--- src/components/offers/OffersListContent.vue
+++ src/components/offers/OffersListContent.vue
@@ -7,15 +7,18 @@ defineProps({
 });
 </script>
 <template>
-  <div v-if="offers && offers.length > 0">
-    <div v-for="(offer, index) in offers" :key="offer.id">
-      <OffersListCard
-        :class="
-          'border-l-2 border-r-2 border-b-2 ' +
-          (index === 0 ? 'border-t-2' : '')
-        "
-        :offer="offer"
-      />
+  <div class="rounded-xl">
+    <div v-if="offers && offers.length > 0">
+      <div v-for="(offer, index) in offers" :key="offer.id">
+        <OffersListCard
+          :class="
+            'border-l-2 border-r-2 border-b-2 ' +
+            (index === 0 ? 'border-t-2 rounded-t-xl ' : '') +
+            (index === offers.length - 1 ? 'rounded-b-xl' : '')
+          "
+          :offer="offer"
+        />
+      </div>
     </div>
   </div>
 </template>
