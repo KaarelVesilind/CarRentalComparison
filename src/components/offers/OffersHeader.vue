73c7031d5a21ebf4c2d544aafe48e26e2575c4be
Kaarel Vesilind <kaarel.vesilind@gmail.com>
2022-11-09 15:06:21 +0200

    only let insert numbers into input field

--- src/components/offers/OffersHeader.vue
+++ src/components/offers/OffersHeader.vue
@@ -1,6 +1,28 @@
 <script setup>
 import OffersFilter from "@/components/offers/OffersFilter.vue";
 import OffersSearch from "@/components/offers/OffersSearch.vue";
+import { useStore } from "@/stores/rental";
+import { ref, computed } from "vue";
+const store = useStore();
+const searchParamsObj = ref(store.searchParamsObj);
+const invalidValues = computed(() => {
+  return (
+    searchParamsObj.value.days < 0 ||
+    searchParamsObj.value.hours < 0 ||
+    searchParamsObj.value.minutes < 0 ||
+    searchParamsObj.value.minutes > 59 ||
+    searchParamsObj.value.hours > 23 ||
+    searchParamsObj.value.distance < 0
+  );
+});
+
+const emit = defineEmits(["search"]);
+const search = () => {
+  if (invalidValues.value) {
+    return;
+  }
+  emit("search");
+};
 </script>
 
 <template>
@@ -9,9 +31,13 @@ import OffersSearch from "@/components/offers/OffersSearch.vue";
     <OffersFilter />
     <button
       class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      @click="$emit('search')"
+      :class="invalidValues ? 'opacity-50 cursor-not-allowed' : ''"
+      @click="search"
     >
       Search
     </button>
+    <p v-if="invalidValues" class="text-red-500 font-bold">
+      Invalid time or distance
+    </p>
   </div>
 </template>
