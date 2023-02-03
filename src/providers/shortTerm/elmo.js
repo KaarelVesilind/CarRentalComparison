66274522d2e055e80503778ec2eb95673445aecf
Kaarel Vesilind <kaarel.vesilind@gmail.com>
2023-02-03 20:07:32 +0200

    updated prices. Some cars not updated. Check TODOs

--- src/providers/shortTerm/elmo.js
+++ src/providers/shortTerm/elmo.js
@@ -9,21 +9,21 @@ export const elmo = {
       gearbox: "AUTOMATIC",
       imageUrl: "providers/elmo/cars/VW-e-Up20.png",
       price: {
-        km: 0.2,
+        km: 0.23,
         minute: 0.11,
-        hour: 6.6,
-        day: 33,
+        hour: 4.95,
+        day: 29.7,
       },
       packages: [
         {
           days: 7,
           distance: 700,
-          price: 225,
+          price: 220,
         },
         {
           days: 30,
           distance: 3000,
-          price: 700,
+          price: 799,
         },
       ],
     },
@@ -35,16 +35,16 @@ export const elmo = {
       gearbox: "AUTOMATIC",
       imageUrl: "providers/elmo/cars/Skoda-Citigo.png",
       price: {
-        km: 0.2,
+        km: 0.23,
         minute: 0.11,
-        hour: 6.6,
-        day: 33,
+        hour: 4.95,
+        day: 29.7,
       },
       packages: [
         {
           days: 7,
           distance: 700,
-          price: 225,
+          price: 220,
         },
         {
           days: 30,
@@ -61,42 +61,16 @@ export const elmo = {
       gearbox: "AUTOMATIC",
       imageUrl: "providers/elmo/cars/Toyota-Yaris-Hybrid.png",
       price: {
-        km: 0.2,
+        km: 0.23,
         minute: 0.11,
-        hour: 6.6,
-        day: 33,
+        hour: 4.95,
+        day: 29.7,
       },
       packages: [
         {
           days: 7,
           distance: 700,
-          price: 249,
-        },
-        {
-          days: 30,
-          distance: 3000,
-          price: 799,
-        },
-      ],
-    },
-    {
-      name: "Nissan Leaf",
-      bodyType: "HATCHBACK",
-      motorType: "ELECTRIC",
-      petFriendly: true,
-      gearbox: "AUTOMATIC",
-      imageUrl: "providers/elmo/cars/Nissan-Leaf.png",
-      price: {
-        km: 0.2,
-        minute: 0.11,
-        hour: 6.6,
-        day: 33,
-      },
-      packages: [
-        {
-          days: 7,
-          distance: 700,
-          price: 249,
+          price: 220,
         },
         {
           days: 30,
@@ -113,16 +87,16 @@ export const elmo = {
       gearbox: "AUTOMATIC",
       imageUrl: "providers/elmo/cars/VW-e-Up20.png",
       price: {
-        km: 0.2,
+        km: 0.23,
         minute: 0.11,
-        hour: 6.6,
-        day: 33,
+        hour: 4.95,
+        day: 29.7,
       },
       packages: [
         {
           days: 7,
           distance: 700,
-          price: 260,
+          price: 220,
         },
         {
           days: 30,
@@ -139,21 +113,21 @@ export const elmo = {
       gearbox: "AUTOMATIC",
       imageUrl: "providers/elmo/cars/Renault-Clio-Hybrid.png",
       price: {
-        km: 0.2,
+        km: 0.23,
         minute: 0.11,
-        hour: 6.6,
-        day: 33,
+        hour: 4.95,
+        day: 29.7,
       },
       packages: [
         {
           days: 7,
           distance: 700,
-          price: 260,
+          price: 220,
         },
         {
           days: 30,
           distance: 3000,
-          price: 825,
+          price: 799,
         },
       ],
     },
@@ -165,16 +139,42 @@ export const elmo = {
       gearbox: "AUTOMATIC",
       imageUrl: "providers/elmo/cars/Renault-Zoe.png",
       price: {
-        km: 0.2,
-        minute: 0.13,
-        hour: 7.8,
-        day: 39,
+        km: 0.23,
+        minute: 0.11,
+        hour: 4.95,
+        day: 29.7,
       },
       packages: [
         {
           days: 7,
           distance: 700,
-          price: 275,
+          price: 220,
+        },
+        {
+          days: 30,
+          distance: 3000,
+          price: 799,
+        },
+      ],
+    },
+    {
+      name: "Nissan Leaf",
+      bodyType: "HATCHBACK",
+      motorType: "ELECTRIC",
+      petFriendly: true,
+      gearbox: "AUTOMATIC",
+      imageUrl: "providers/elmo/cars/Nissan-Leaf.png",
+      price: {
+        km: 0.23,
+        minute: 0.13,
+        hour: 6.5,
+        day: 32.5,
+      },
+      packages: [
+        {
+          days: 7,
+          distance: 700,
+          price: 245,
         },
         {
           days: 30,
@@ -191,21 +191,21 @@ export const elmo = {
       gearbox: "AUTOMATIC",
       imageUrl: "providers/elmo/cars/Peugeot-e-208-GT.png",
       price: {
-        km: 0.2,
+        km: 0.23,
         minute: 0.13,
-        hour: 7.8,
-        day: 39,
+        hour: 6.5,
+        day: 32.5,
       },
       packages: [
         {
           days: 7,
           distance: 700,
-          price: 275,
+          price: 245,
         },
         {
           days: 30,
           distance: 3000,
-          price: 890,
+          price: 825,
         },
       ],
     },
@@ -217,33 +217,33 @@ export const elmo = {
       gearbox: "AUTOMATIC",
       imageUrl: "providers/elmo/cars/Renault-Arkana-Hybrid.png",
       price: {
-        km: 0.22,
+        km: 0.23,
         minute: 0.15,
-        hour: 9.0,
-        day: 45,
+        hour: 7.95,
+        day: 39.75,
       },
       packages: [
         {
           days: 7,
           distance: 700,
-          price: 350,
+          price: 275,
         },
         {
           days: 30,
           distance: 3000,
-          price: 890,
+          price: 825,
         },
       ],
     },
     {
-      name: "Tesla Model 3 SR+",
+      name: "Tesla Model 3",
       bodyType: "SEDAN",
       motorType: "ELECTRIC",
       petFriendly: true,
       gearbox: "AUTOMATIC",
       imageUrl: "providers/elmo/cars/Tesla-Model-3-SR+.png",
       price: {
-        km: 0.2,
+        km: 0.24,
         minute: 0.25,
         hour: 15,
         day: 75,
@@ -258,6 +258,33 @@ export const elmo = {
           days: 30,
           distance: 3000,
           price: 1599,
+        },
+      ],
+    },
+    {
+      name: "Tesla Model Y",
+      bodyType: "SEDAN",
+      motorType: "ELECTRIC",
+      petFriendly: true,
+      gearbox: "AUTOMATIC",
+      // TODO add the right image from app
+      imageUrl: "providers/elmo/cars/Tesla-Model-3-SR+.png",
+      price: {
+        km: 0.24,
+        minute: 0.35,
+        hour: 21,
+        day: 105,
+      },
+      packages: [
+        {
+          days: 7,
+          distance: 700,
+          price: 750,
+        },
+        {
+          days: 30,
+          distance: 3000,
+          price: 2600,
         },
       ],
     },
